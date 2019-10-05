03b7d66a1099c3d35977646cb000a8fb72f58921
cfbender <codyfbender@gmail.com>
2019-10-04 23:00:54 -0600

    Add styling and full form validation. Prep Modal

--- client/src/Question.tsx
+++ client/src/Question.tsx
@@ -1,25 +1,31 @@
 import React from "react";
 import { Select } from "react-materialize";
 
-const Question: React.FC<{ text: string; callback: Function }> = ({
-  text,
-  callback
-}) => {
+const Question: React.FC<{
+  name: string;
+  text: string;
+  callback: Function;
+  errors: string;
+  selected: number;
+}> = ({ name, text, callback, errors, selected }) => {
   return (
-    <div>
-      <div className="input-field col s12">
-        <Select id="one" value="" onChange={callback}>
-          <option value="" disabled>
-            Choose your option
-          </option>
-          <option value="1">1 (strongly disagree)</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5 (strongly agree)</option>
-        </Select>
-        <label>{text}</label>
-      </div>
+    <div className="col s12">
+      <Select
+        id={name}
+        label={text}
+        value={selected.toString() || ""}
+        onChange={callback}
+      >
+        <option value="0" disabled>
+          Choose your option
+        </option>
+        <option value="1">1 (strongly disagree)</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5 (strongly agree)</option>
+      </Select>
+      <span className="question-error">{errors}</span>
     </div>
   );
 };
--- client/src/Survey.tsx
+++ client/src/Survey.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { TextInput } from "react-materialize";
+import { TextInput, Modal } from "react-materialize";
 import Question from "./Question";
+import "./Survey.css";
 
 const Survey: React.FC = () => {
   const [answers, updateAnswers]: [
@@ -18,35 +19,98 @@ const Survey: React.FC = () => {
     nine: 0,
     ten: 0
   });
+
+  const [name, updateName]: [
+    { text?: string; error?: string },
+    Function
+  ] = useState({});
+  const [picture, updatePicture]: [
+    { text?: string; error?: string },
+    Function
+  ] = useState({});
+
   const questions: string[] = [
     "You prefer to stay at home rather than go out and party.",
-    "You prefer cats over dogs"
+    "You prefer cats over dogs",
+    "You love the great outdoors",
+    "You'll never spend more than $2.99 on a draft beer",
+    "When you lift your arms up, it looks like you've been swimming in shoulder-height water",
+    "You'll wear yesterday's underwear",
+    "You can't function without at least 3 cups of coffee",
+    "You are totally fine to poop in public",
+    "You think double-dipping is A-okay",
+    "Your name is Ben Shapiro"
   ];
+  interface FriendMatch {
+    show: boolean;
+    data?: { name: string; photo: string };
+  }
+
+  const [match, updateMatch]: [FriendMatch, Function] = useState({
+    show: false
+  });
+
+  const [errors, updateErrors]: [{ [k: string]: string }, Function] = useState(
+    {}
+  );
+
+  const formValidate = (): boolean => {
+    updateErrors({});
+    let formIsValid = true;
+    let temp: { [k: string]: string } = {};
+    Object.keys(answers).forEach(prop => {
+      if (answers[prop] === 0) {
+        formIsValid = false;
+        temp[prop] = "You must select an answer";
+        updateErrors(temp);
+      }
+    });
+    if (!name.text) {
+      formIsValid = false;
+      updateName({ error: "You must enter a name." });
+    }
+    if (!picture.text) {
+      formIsValid = false;
+      updatePicture({ error: "You must enter a photo link." });
+    }
+    return formIsValid;
+  };
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const url = "/api";
-    const data = { answers: getAnswers() };
-    console.log(data);
-    try {
-      const response = await fetch(url, {
-        method: "POST", // or 'PUT'
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      const json = await response.json();
-      console.log("Success:", JSON.stringify(json));
-    } catch (error) {
-      console.log("Error", error);
+    if (formValidate()) {
+      const url = "/api";
+      const data = { answers: getAnswers() };
+      console.log(data);
+      try {
+        const response = await fetch(url, {
+          method: "POST", // or 'PUT'
+          body: JSON.stringify(data), // data can be `string` or {object}!
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+
+        const json = await response.json();
+        json.show = true;
+        updateMatch(json);
+      } catch (error) {
+        console.log("Error", error);
+      }
     }
   };
 
-  const handleChange = (e: { target: { id: string; value: number } }) => {
-    const temp: { [k: string]: number } = answers;
+  const handleChange = (e: { target: { id: number; value: number } }) => {
+    console.log(name);
+    formValidate();
+    console.log(name);
+    const temp = answers;
     const id = e.target.id;
     temp[id] = e.target.value;
+    const tempErrors = errors;
+    tempErrors[id] = "";
     updateAnswers(temp);
+    updateErrors(tempErrors);
   };
 
   const getAnswers = () => {
@@ -58,8 +122,31 @@ const Survey: React.FC = () => {
   return (
     <div className="container survey">
       <h2>About You</h2>
-      <TextInput placeholder="Name (Required)" />
-      <TextInput placeholder="Link to Picture of You (Required)" />
+      <Modal header="Modal Header" open={match.show}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </Modal>
+      <TextInput
+        placeholder="Name (Required)"
+        onChange={(e: { target: { value: string } }) =>
+          updateName({ text: e.target.value })
+        }
+      />
+      <p className="text-error">{name.error}</p>
+      <TextInput
+        placeholder="Link to Picture of You (Required)"
+        onChange={(e: { target: { value: string } }) =>
+          updatePicture({ text: e.target.value })
+        }
+      />
+      <p className="text-error">{picture.error}</p>
       <h2>Survey:</h2>
       <p>
         Rate how much you agree with the following statements, from a 1
@@ -67,15 +154,23 @@ const Survey: React.FC = () => {
       </p>
       <div className="row">
         <form onSubmit={handleSubmit}>
-          {questions.map((question, idx) => {
-            return (
-              <Question
-                key={idx}
-                text={questions[idx]}
-                callback={handleChange}
-              />
-            );
-          })}
+          <label>
+            {questions.map((question, idx) => {
+              let prop = Object.keys(answers)[idx];
+              return (
+                <div className="question row" key={`div${idx + 1}`}>
+                  <Question
+                    key={idx + 1}
+                    name={prop}
+                    text={questions[idx]}
+                    callback={handleChange}
+                    errors={errors[prop]}
+                    selected={answers[prop]}
+                  />
+                </div>
+              );
+            })}
+          </label>
           <input
             id="survey-submit"
             type="submit"
